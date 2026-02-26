0c2989f0bb2fa610fa2820ccf3e4a6640ee32fe2
Ángel Socias <angelsocias@gmail.com>
2026-02-26 18:18:26 +0100

    change more colors

--- src/index.ts
+++ src/index.ts
@@ -420,25 +420,21 @@ align="center">
             </tr>
             <!-- Email Body -->
             <tr>
-              <td class="email-body" width="100%" cellpadding="0" cellspacing="0" style="word-break: break-word; font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 16px; width: 
-100%; margin: 0; padding: 0; -premailer-width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0;">
-                <table class="email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="width: 570px; margin: 0 auto; padding: 0; -premailer-width: 
-570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; background-color: #FFFFFF;" bgcolor="#FFFFFF">
+              <td class="email-body" width="100%" cellpadding="0" cellspacing="0" style="word-break: break-word; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 16px; width: 100%; margin: 0; padding: 0; -premailer-width: 100%; -premailer-cellpadding: 0; -premailer-cellspacing: 0;">
+                <table class="email-body_inner" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="width: 570px; margin: 0 auto; padding: 0; -premailer-width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; background-color: #ffffff;" bgcolor="#ffffff">
                   <!-- Body content -->
                   <tr>
-                    <td class="content-cell" style="word-break: break-word; font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 16px; padding: 24px;">
+                    <td class="content-cell" style="word-break: break-word; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 16px; padding: 28px;">
                       <div class="f-fallback">
-                        <p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #51545E;">Hola {{ .Data.display_name }},</p>
-<p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #51545E;">Estás a un paso de confirmar tu cuenta de JAMIT. Por favor,haz click en el botón de aquí abajo para confirmar 
-tu dirección de correo electrónico.</p>
+                        <p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #44403c;">Hola {{ .Data.display_name }},</p>
+<p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #44403c;">Estás a un paso de confirmar tu cuenta de JAMIT. Por favor,haz click en el botón de aquí abajo para confirmar tu dirección de correo electrónico.</p>
 
-
+      
       <!--[if mso]>
       <div align="center">
-        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://localhost:1234/auth/confirm?token_hash={{ .TokenHash }}&type=signup" 
-style="height:36px;v-text-anchor:middle;width:200px;" arcsize="8.33%" stroke="f" fillcolor="#6000C7">
+        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="{{ .ConfirmationURL }}" style="height:36px;v-text-anchor:middle;width:200px;" arcsize="16.67%" stroke="f" fillcolor="#ea580c">
           <w:anchorlock/>
-          <center style="color:#FFFFFF;font-family:'Nunito Sans', Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;">
+          <center style="color:#ffffff;font-family:'Lato', Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;">
             Activar Cuenta
           </center>
         </v:roundrect>
@@ -447,28 +443,20 @@ style="height:36px;v-text-anchor:middle;width:200px;" arcsize="8.33%" stroke="f"
       <!--[if !mso]><!-- -->
       <table style="margin: 30px auto; width: auto;" border="0" cellspacing="0" cellpadding="0" role="presentation">
         <tr>
-          <td align="center" bgcolor="#6000C7" style="word-break: break-word; font-family: 'Nunito Sans', Helvetica, Arial, sans-serif; font-size: 16px; border-radius: 3px; background-color: 
-#6000C7;">
-            <a href="http://localhost:1234/auth/confirm?token_hash={{ .TokenHash }}&type=signup" target="_blank" rel="noopener noreferrer" class="button" style="box-shadow: 0 2px 3px rgba(0, 0, 0, 
-0.16); -webkit-text-size-adjust: none; border: solid 1px #6000C7; border-radius: 3px; box-sizing: border-box; cursor: pointer; font-size: 16px; font-weight: bold; margin: 0; padding: 10px 20px; 
-text-transform: capitalize; border-color: #6000C7; text-align: center; display: inline-block; background-color: #6000C7 !important; border-top: 10px solid #6000C7 !important; border-right: 18px 
-solid #6000C7 !important; border-bottom: 10px solid #6000C7 !important; border-left: 18px solid #6000C7 !important; color: #FFFFFF !important; text-decoration: none !important;">
+          <td align="center" bgcolor="#ea580c" style="word-break: break-word; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 16px; border-radius: 6px; background-color: #ea580c;">
+            <a href="{{ .ConfirmationURL }}" target="_blank" rel="noopener noreferrer" class="button" style="box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); -webkit-text-size-adjust: none; border: solid 1px #ea580c; border-radius: 6px; box-sizing: border-box; cursor: pointer; font-size: 16px; font-weight: bold; margin: 0; padding: 10px 20px; text-transform: capitalize; border-color: #ea580c; text-align: center; display: inline-block; background-color: #ea580c !important; border-top: 10px solid #ea580c !important; border-right: 18px solid #ea580c !important; border-bottom: 10px solid #ea580c !important; border-left: 18px solid #ea580c !important; color: #ffffff !important; text-decoration: none !important;">
               Activar Cuenta
             </a>
           </td>
         </tr>
       </table>
       <!--<![endif]-->
-    <p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #51545E;">Si no te has creado una cuenta en JAMIT, puedes ignorar este mensaje. (O acceder, y ver que se cuece)</p>
-<p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #51545E;">¿Necesitas ayuda? Puedes encontrarnos en hola@jamit.es</p><p style="margin: .4em 0 1.1875em; font-size: 16px; 
-line-height: 1.625; color: #51545E;">¡Gracias!<br>JAMIT Team</p>
+    <p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #44403c;">Si no te has creado una cuenta en JAMIT, puedes ignorar este mensaje. (O acceder, y ver que se cuece)</p>
+<p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #44403c;">¿Necesitas ayuda? Puedes encontrarnos en hola@jamit.es</p><p style="margin: .4em 0 1.1875em; font-size: 16px; line-height: 1.625; color: #44403c;">¡Gracias!<br>JAMIT Team</p>
 
-      <div class="fallback-text" style="margin-top: 40px; padding-top: 25px; border-top: 1px solid #ECEDEE;">
-        <p class="f-fallback sub" style="margin: .2em 0; color: #A8AAAF; font-size: 12px; line-height: 1.4;">If you're having trouble with the button above, copy and paste the URL below into your 
-web browser.</p>
-        <p class="f-fallback sub" style="margin: .2em 0; color: #A8AAAF; font-size: 12px; line-height: 1.4; margin-top: .1em;"><a href="http://localhost:1234/auth/confirm?token_hash={{ .TokenHash 
-}}&type=signup" class="fallback-link" style="color: #A8AAAF; text-decoration: underline; word-break: break-all; font-size: 12px;">http://localhost:1234/auth/confirm?token_hash={{ .TokenHash 
-}}&type=signup</a></p>
+      <div class="fallback-text" style="margin-top: 40px; padding-top: 25px; border-top: 1px solid #fed7aa;">
+        <p class="f-fallback sub" style="margin: .2em 0; color: #78716c; font-size: 12px; line-height: 1.4;">Si tienes problemas con el botón, copia y pega este enlace en tu navegador.</p>
+        <p class="f-fallback sub" style="margin: .2em 0; color: #78716c; font-size: 12px; line-height: 1.4; margin-top: .1em;"><a href="{{ .ConfirmationURL }}" class="fallback-link" style="color: #78716c; text-decoration: underline; word-break: break-all; font-size: 12px;">{{ .ConfirmationURL }}</a></p>
       </div>
 
                         <!-- Optional fallback text section -->
